d4015656c18dcf9e15279b55e3aac58147882b01
ChayaGayer <chayagayer@gmail.com>
2022-10-31 17:56:27 +0200

    welcome stage

---
+++ doNet5783_9718_9700/Stage0/Program9718.cs
@@ -0,0 +1,24 @@
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+
+namespace Stage0
+{
+    partial class program
+    {
+        static void Main(String[]args)
+        {
+            welcome9718();
+            welcome9700();
+            Console.ReadKey();
+        }
+        static partial void welcome9700();
+        private static void welcome9718()
+        {
+            string x;
+            Console.Write("Enter your name: ");
+            x = Console.ReadLine();
+            Console.WriteLine("{0} welcome to my first console application", x);
+        }
+    }
+}
