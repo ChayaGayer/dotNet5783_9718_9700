c1e3bcbe704e011e85a3ed39d429af59e3a836d2
ChayaGayer <chayagayer@gmail.com>
2022-11-10 21:19:09 +0200

    first steps at stage 1

---
+++ DalFacade/DO/Order.cs
@@ -0,0 +1,31 @@
+﻿
+
+using System.Diagnostics;
+using System.Xml.Linq;
+
+namespace DO;
+
+public struct Order
+{
+    public int ID { get; set; }
+    public string? CustomerName { get; set; }
+    public string? CustomerEmail { get; set; }
+    public string? CustomerAddress { get; set; }
+    public DateTime? OrderDate { get; set; }
+    public DateTime? ShipDate { get; set; }
+    public DateTime? DeliveryDate { get; set; }
+    public override string ToString() => $@"
+Product ID={ID}, 
+CustomerName ={CustomerName},
+CustomerEmail={CustomerEmail},
+ CustomerAddress={CustomerAddress},
+OrderDate={OrderDate},
+ShipDate={ShipDate},
+DeliveryDate={DeliveryDate}
+";
+
+
+
+
+
+}
---
+++ Stage0/Program9700.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Stage0
+{
+   
+   partial class program
+    {
+        static partial void welcome9700()
+        {
+            Console.WriteLine("I am also here!");
+        }
+    }
+}
